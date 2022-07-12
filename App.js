68fbce44109ae6334d08ac05c47caf485be89f33
Cr7Mujassam <106069072+Cr7Mujassam@users.noreply.github.com>
2022-07-12 17:26:19 +0530

    Update App.js

--- App.js
+++ App.js
@@ -20,3 +20,38 @@ function App() {
   );
 }
 export default App;
+
+
+
+
+Another that we fetch the data using useRef() and displaying the data in Browser using useState()
+
+the Code is here
+
+
+import './App.css';
+import React, {useRef,useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  
+  const [name,setName] = useState('');
+  const initialRef = useRef();
+
+  function click(){
+    const enteredValue = initialRef.current.value;
+    setName(enteredValue);
+    console.log(enteredValue);
+  }
+ 
+  return (
+    <div className="container-fluid"> 
+      <label>Name</label>
+      <input ref={initialRef}  type="text" placeholder='Name'></input>
+      <h1>{name}</h1>
+      <button onClick={click} type="button">Submit</button>
+    </div>
+  );
+}
+export default App;
+
